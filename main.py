10f70462631430086398c8dab2e08a3b242a9f61
Robert Ellis <rob.ellis357@gmail.com>
2025-03-20 08:53:15 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,12 +9,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (0,0,0)
+    clock = pygame.time.Clock()
+    dt = 0.016
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2,PLAYER_RADIUS)
+    #Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,color)
         pygame.display.flip()
+        clock.tick(60)
+        player.draw(screen)
         
 
 if __name__ == '__main__':
